2f8a4d441ded634bc8f218feb988934338d2c6e4
CodeWithManu2023 <148353270+CodeWithManu2023@users.noreply.github.com>
2023-11-04 23:59:15 +0530

    Part 2 of Angular Tutorials

---
+++ Part 2 -  Component/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { ReportsComponent } from './page/reports/reports.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    ReportsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
